53a2e3ae3dda73d520017af8759c83bf5fa9fc99
Prashant <101106030+Prashu445@users.noreply.github.com>
2025-12-31 06:05:39 +0000

    Add PWA manifest

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,14 @@ export const metadata = {
   description: 'E2EE Private Messaging',
 }
 
+export const viewport = {
+  themeColor: 'black',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
